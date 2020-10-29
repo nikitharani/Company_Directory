d65b30b1073d28eb3ed433ddb5dd4047e348cfae
Nikitha <nikithanalumachu@gmail.com>
2020-10-29 13:09:44 +0000

    added php file

--- libs/php/getAll.php
+++ libs/php/getAll.php
@@ -34,7 +34,7 @@
 
 	}	
 
-	$query = 'SELECT p.lastName, p.firstName, p.jobTitle, p.email, d.name as department, l.name as location FROM personnel p LEFT JOIN department d ON (d.id = p.departmentID) LEFT JOIN location l ON (l.id = d.locationID) ORDER BY p.lastName, p.firstName, d.name, l.name';
+	$query = 'SELECT p.id, p.lastName, p.firstName, p.jobTitle, p.email, d.name as department, l.name as location FROM personnel p LEFT JOIN department d ON (d.id = p.departmentID) LEFT JOIN location l ON (l.id = d.locationID) ORDER BY p.id, p.lastName, p.firstName, d.name, l.name';
 
 	$result = $conn->query($query);
 	
@@ -53,12 +53,16 @@
 
 	}
    
-   	$data = [];
+	   $data = [];
+	   $counter=0;
 
 	while ($row = mysqli_fetch_assoc($result)) {
 
 		array_push($data, $row);
-
+// 		if($counter==20){
+// 		break;
+// 		}
+// $counter++;
 	}
 
 	$output['status']['code'] = "200";
